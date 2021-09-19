5952093e4e13d1f5098e08daefade6dce275a266
Shahid-sk <mail2sk.shahid@gmail.com>
2021-09-19 22:09:15 +0530

    added: sequence problems using if-else

---
+++ SP with if and else/CoinFlip.js
@@ -0,0 +1,8 @@
+const randomNum = (Math.floor(Math.random() * 2))
+
+if(randomNum == 1){
+    console.log("Heads")
+}
+else{
+    console.log("Tails")
+}
---
+++ SP with if and else/MaxMin.js
@@ -0,0 +1,27 @@
+const MAXCOUNT = 5
+let count = 0
+const number = []
+
+// Generating 5 random numbers.
+while(count < MAXCOUNT){
+    number[count] = (Math.floor(Math.random() * 900)+100)
+    count++;
+}
+console.log("Random numbers are: " + number)
+
+let max = number[1]
+let min = number[1]
+
+// Getting the maximum and minimum numbers.
+for( i = 0; i<number.length; i++){
+
+    if(number[i] > max){
+        max = number[i]
+    }
+
+    if(number[i] < min){
+        min = number[i]
+    }
+}
+console.log("Maximum number is: " + max)
+console.log("Minimum number is: " + min)
